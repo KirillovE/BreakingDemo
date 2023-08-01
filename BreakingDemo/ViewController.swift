af288a329a4ab6d2f965d7827c772b030ecf23a2
Eugène KIRILLOV <e.kirillov@betclicgroup.com>
2023-08-01 17:04:28 +0200

    Base setup

--- BreakingDemo/ViewController.swift
+++ BreakingDemo/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func leftButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func rightButtonTapped(_ sender: Any) {
+    }
 }
 
